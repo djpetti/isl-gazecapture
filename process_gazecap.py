c38c6b297393b67c26002aafcdbd508db08bd3d8
Daniel Petti <djpetti@gmail.com>
2018-02-13 09:36:10 -0500

    Remove unecessary code in process_gazecap.

--- process_gazecap.py
+++ process_gazecap.py
@@ -38,44 +38,6 @@ def extract_crop_data(crop_info):
 
   return x_crop, y_crop, w_crop, h_crop, crop_valid
 
-def extract_eye_crops(images, face_data, left_eye_data):
-  """ Extract the eye crop from an image.
-  Args:
-    images: A list of images to process.
-    left_eye_data: The crop data for the left eye.
-    face_data: The crop data for the face.
-  Returns:
-    A cropped version of the images. """
-  face_x = face_data['X']
-  face_y = face_data['Y']
-
-  eye_x = left_eye_data['X']
-  eye_y = left_eye_data['Y']
-  eye_w = left_eye_data['W']
-  eye_h = left_eye_data['H']
-
-  crops = []
-  for fx, fy, ex, ey, ew, eh, image in zip(face_x, face_y, eye_x,
-                                           eye_y, eye_w, eye_h, images):
-    # First, get the global eye crop position.
-    global_x = int(fx + ex)
-    global_y = int(fy + ey)
-    end_x = int(global_x + ew)
-    end_y = int(global_y + eh)
-
-    # Now, crop the image.
-    crop = image[global_y:end_y, global_x:end_x]
-    if not crop.shape[0]:
-      # Still add it as a placeholder.
-      crops.append(crop)
-      continue
-
-    # Resize the crop.
-    crop = reshape_image(crop, (60, 36))
-    crops.append(crop)
-
-  return crops
-
 def extract_face_crops(images, face_data):
   """ Extract the face crop from an image.
   Args:
@@ -108,72 +70,6 @@ def extract_face_crops(images, face_data):
     crops.append(crop)
 
   return crops
-
-def reshape_image(image, shape, offset=(0, 0)):
-  """ Reshapes a stored image so that it is a consistent shape and size.
-  Args:
-    image: The image to reshape.
-    shape: The shape we want the image to be.
-    offset: An optional offset. This can be used to direct it not to crop to the
-            center of the image. In the tuple, the horizontal offset comes
-            before the vertical one.
-  Returns:
-    The reshaped image.
-  """
-  # Crop the image to just the center square.
-  if len(image.shape) == 3:
-    # It may have multiple color channels.
-    height, width, _ = image.shape
-  else:
-    height, width = image.shape
-
-  target_width, target_height = shape
-
-  # Find the largest we can make the initial crop.
-  multiplier = 1
-  if width > target_width:
-    multiplier = width / target_width
-  elif height > target_height:
-    multiplier = height / target_height
-  target_width *= multiplier
-  target_height *= multiplier
-
-  crop_width = target_width
-  crop_height = target_height
-  # Our goal here is to keep the same aspect ratio as the original.
-  if width <= target_width:
-    # We need to reduce the width for our initial cropping.
-    crop_width = width
-    crop_height = target_height * (float(crop_width) / target_width)
-  if height <= target_height:
-    # We need to reduce the height for our initial cropping.
-    crop_height = height
-    crop_width = target_width * (float(crop_height) / target_height)
-
-  crop_width = int(crop_width)
-  crop_height = int(crop_height)
-
-  # Crop the image.
-  crop_left = (width - crop_width) / 2
-  crop_top = (height - crop_height) / 2
-
-  # Account for the crop offset.
-  offset_left, offset_top = offset
-  crop_left += offset_left
-  crop_top += offset_top
-  # Bound it in the image.
-  crop_left = max(0, crop_left)
-  crop_left = min(width - 1, crop_left)
-  crop_top = max(0, crop_top)
-  crop_top = min(height - 1, crop_top)
-
-  image = image[crop_top:(crop_height + crop_top),
-                crop_left:(crop_width + crop_left)]
-
-  # Set a proper size, which should just be directly scaling up or down.
-  image = cv2.resize(image, shape)
-
-  return image
 
 def generate_names(dot_info, grid_info, face_info, left_eye_info,
                    right_eye_info, session):
