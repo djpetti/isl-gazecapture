435512613d995fc53d09f088fb3c612f69068599
Daniel Petti <djpetti@gmail.com>
2018-02-15 12:45:24 -0500

    Add user-friendly demo mode and logging.

--- server/main.py
+++ server/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
+
+import argparse
+import logging
+
 import cv2
 
 import server
 
 
-def main():
+# TODO (daniel) Logging to a file eventually.
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s",
+                    level=logging.INFO)
+
+
+def demo():
+  """ A demo version of the server that displays received images on-screen. """
   my_server = server.Server(6219)
   my_server.wait_for_client()
 
@@ -19,6 +29,19 @@ def main():
 
     cv2.imshow("test", frame)
     cv2.waitKey(1)
+
+def main():
+  parser = argparse.ArgumentParser( \
+      description="Run the gaze estimation server.")
+  parser.add_argument("-d", "--demo", action="store_true",
+                      help="Run a demo server that displays received images.")
+  args = parser.parse_args()
+
+  if args.demo:
+    # Run the demo.
+    demo()
+  else:
+    raise NotImplementedError("Full server is not yet implemented.")
 
 
 if __name__ == "__main__":
