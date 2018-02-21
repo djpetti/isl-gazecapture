ea2b847d23efdf6649ca06d2c8eda208aa718ccd
Daniel Petti <djpetti@gmail.com>
2018-02-21 14:35:22 -0500

    Update server script to use new container.
    Also set it to expose the proper port.

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull djpetti/rpinets-tensorflow
+docker pull djpetti/isl-gazecapture
 
-nvidia-docker run --rm -v `pwd`:/server_dir \
-    djpetti/rpinets-tensorflow /usr/bin/python /server_dir/main.py
+nvidia-docker run --rm -v `pwd`:/server_dir -p 6219:6219 \
+    djpetti/isl-gazecapture /usr/bin/python /server_dir/main.py
