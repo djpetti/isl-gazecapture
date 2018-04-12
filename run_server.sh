4e32caa44ca9fc511d5b94ae5c4620307ddb2f97
Daniel Petti <djpetti@gmail.com>
2018-04-12 09:15:32 -0400

    Modify docker script for new CLI argument.
    It transparently passes its first argument to the server script
    as the phone configuration to use.

--- run_server.sh
+++ run_server.sh
@@ -4,4 +4,4 @@ docker pull djpetti/isl-gazecapture
 
 nvidia-docker run --rm -v `pwd`:/server_dir -p 6219:6219 \
     djpetti/isl-gazecapture bash -c \
-    "cd /server_dir && /usr/bin/python server_main.py"
+    "cd /server_dir && /usr/bin/python server_main.py $1"
