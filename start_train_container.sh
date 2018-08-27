f887171cf06c38430bcffa8003410feba4fcdc95
Daniel Petti <djpetti@gmail.com>
2018-08-27 12:31:23 -0400

    Add missing script.
    It interactively starts a container for training, and was
    mentioned in the documentation. I forgot to add it before.

---
+++ start_train_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull djpetti/isl-gazecapture
+
+nvidia-docker run -ti --rm --net=host \
+  -v `pwd`:/root/isl_gazecapture \
+  -v /training_data/:/training_data djpetti/isl-gazecapture \
+  /bin/bash
