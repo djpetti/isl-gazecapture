b8489ccfc97e671036c29531b2119a39ea94ef8a
Daniel Petti <djpetti@gmail.com>
2018-10-03 11:06:30 -0400

    Add initial support for branching architecture.
    This one branches into two groups based on the face area.

--- itracker/training/validator.py
+++ itracker/training/validator.py
@@ -36,10 +36,14 @@ class Validator(object):
     self.__build_analysis_graph()
 
   def __build_model(self):
-    """ Builds the model and loads the model weights. """
+    """ Builds the model and loads the model weights. It also modifies
+    self.__labels according to the model. """
     # Create the model.
     net = config.NET_ARCH(config.FACE_SHAPE, eye_shape=config.EYE_SHAPE)
     self.__model = net.build()
+
+    # Prepare the label data.
+    self.__labels = net.prepare_labels(self.__labels)
 
     logger.info("Loading pretrained model '%s'." % (self.__save_path))
     self.__model.load_weights(self.__save_path)
